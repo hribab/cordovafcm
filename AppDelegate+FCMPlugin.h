6fcefc8805332b822292be4e82b2004f0134025e
hribab <itzhari.g@gmail.com>
2017-11-21 00:07:27 -0800

    first commit

---
+++ AppDelegate+FCMPlugin.h
@@ -0,0 +1,31 @@
+//
+//  AppDelegate+FCMPlugin.h
+//  TestApp
+//
+//  Created by felipe on 12/06/16.
+//
+//
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVViewController.h>
+
+#import "Firebase.h"
+
+// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
+#ifndef NSFoundationVersionNumber_iOS_9_x_Max
+#define NSFoundationVersionNumber_iOS_9_x_Max 1299
+#endif
+
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+
+@interface AppDelegate (FCMPlugin) <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
+
+@end
+#else
+
+@interface AppDelegate (FCMPlugin) <FIRMessagingDelegate>
+
+@end
+#endif
---
+++ FCMPlugin.m
@@ -0,0 +1,130 @@
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+#import "AppDelegate+FCMPlugin.h"
+
+#import <Cordova/CDV.h>
+#import "FCMPlugin.h"
+#import "Firebase.h"
+
+@interface FCMPlugin () {}
+@end
+
+@implementation FCMPlugin
+
+static BOOL notificatorReceptorReady = NO;
+
+static NSString *notificationCallback = @"FCMPlugin.onNotificationReceived";
+static NSString *tokenRefreshCallback = @"FCMPlugin.onTokenRefreshReceived";
+static FCMPlugin *fcmPluginInstance;
+
++ (FCMPlugin *) fcmPlugin {
+
+    return fcmPluginInstance;
+}
+
+- (void) ready:(CDVInvokedUrlCommand *)command
+{
+    NSLog(@"Cordova view ready");
+    fcmPluginInstance = self;
+    [self.commandDelegate runInBackground:^{
+
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+
+}
+
+// GET TOKEN //
+- (void) getToken:(CDVInvokedUrlCommand *)command
+{
+    NSLog(@"get Token");
+    [self.commandDelegate runInBackground:^{
+        NSString* token = [FIRMessaging messaging].FCMToken;
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:token];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+// UN/SUBSCRIBE TOPIC //
+- (void) subscribeToTopic:(CDVInvokedUrlCommand *)command
+{
+    NSString* topic = [command.arguments objectAtIndex:0];
+    NSLog(@"subscribe To Topic %@", topic);
+    [self.commandDelegate runInBackground:^{
+        if(topic != nil)[[FIRMessaging messaging] subscribeToTopic:topic];
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:topic];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void) unsubscribeFromTopic:(CDVInvokedUrlCommand *)command
+{
+    NSString* topic = [command.arguments objectAtIndex:0];
+    NSLog(@"unsubscribe From Topic %@", topic);
+    [self.commandDelegate runInBackground:^{
+        if(topic != nil)[[FIRMessaging messaging] unsubscribeFromTopic:topic];
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:topic];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void) registerNotification:(CDVInvokedUrlCommand *)command
+{
+    NSLog(@"view registered for notifications");
+
+    notificatorReceptorReady = YES;
+
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void) notifyOfMessage:(NSData *)payload
+{
+    NSLog(@"notifyOfMessage: => executing javascript callback");
+    NSString *JSONString = [[NSString alloc] initWithBytes:[payload bytes] length:[payload length] encoding:NSUTF8StringEncoding];
+    NSString * notifyJS = [NSString stringWithFormat:@"%@(%@);", notificationCallback, JSONString];
+    NSLog(@"stringByEvaluatingJavaScriptFromString %@", notifyJS);
+
+    if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
+        [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:notifyJS];
+    } else {
+        [self.webViewEngine evaluateJavaScript:notifyJS completionHandler:nil];
+    }
+}
+
+-(void) notifyOfTokenRefresh:(NSString *)token
+{
+    NSString * notifyJS = [NSString stringWithFormat:@"%@('%@');", tokenRefreshCallback, token];
+    NSLog(@"stringByEvaluatingJavaScriptFromString %@", notifyJS);
+
+    if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
+        [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:notifyJS];
+    } else {
+        [self.webViewEngine evaluateJavaScript:notifyJS completionHandler:nil];
+    }
+}
+- (void)messaging:(nonnull FIRMessaging *)messaging didRefreshRegistrationToken:(nonnull NSString *)fcmToken {
+    // Note that this callback will be fired everytime a new token is generated, including the first
+    // time. So if you need to retrieve the token as soon as it is available this is where that
+    // should be done.
+    NSLog(@"FCM registration token: %@", fcmToken);
+
+    // TODO: If necessary send token to application server.
+}
+-(void) appEnterBackground
+{
+    NSLog(@"Set state background");
+}
+
+-(void) appEnterForeground
+{
+    NSLog(@"Set state foreground");
+
+}
+@end
